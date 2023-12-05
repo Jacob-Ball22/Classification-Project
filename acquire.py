17bb3064f1d8f2f50692809437c371f4d4725cd5
Jacob-Ball22 <jeball22@gmail.com>
2023-12-05 10:57:04 -0600

    Finalized docstrings for functions in prepare and acquire

--- acquire.py
+++ acquire.py
@@ -5,6 +5,12 @@ import os
 #The function get_telco_data() gets the telco data from the CodeUp MySQL database
 
 def get_telco_data():
+    """
+    Takes in no data just use acquire.get_telco_data()
+    Looks for a csv file of the telco data and uses that if available
+    If not, it pulls the data from the CodeUp MySQL database
+    Turns the SQL database into a csv file to easily reread the data
+    """
     filename = 'telco.csv'
     if os.path.exists(filename):
         print('this file exists, reading from csv')
--- prepare.py
+++ prepare.py
@@ -6,26 +6,65 @@ import os
 import acquire
 
 
-def prep_and_split_telco(telco_df, col):
+def prep_telco(df):
     """
     Drops columns 'payment_type_id', 'internet_service_type_id', and 'contract_type_id'
-    Recieves dataframe as 'df' and target variable to stratify as 'col'
-    First split does a 60% train and 40% validate
-    Second split uses the 40% validate to make 50% validate and 50% test
+    Takes 'internet_service_type' and fills in all the Null values with None
+    Takes away an extra space infront of the data for total_charges
+    Adds a zero to toal_charges to allow it to become a type float
+    Returns the new prepared data
     """
-    telco_df = telco_df.drop(columns=['payment_type_id','internet_service_type_id','contract_type_id'])
+    df = df.drop(columns=['payment_type_id','internet_service_type_id','contract_type_id'])
+    df['internet_service_type'] = df['internet_service_type'].fillna('none')
+    df.total_charges = df.total_charges.str.replace(' ','0.0')
+    df['total_charges'] = (df.total_charges + '0').astype(float)
+    return df
+
+def split_data(df, col):
+    """
+    Takes in dataframe and targer column
+    Splits the data into 60% Train and 40% Validate_Test
+    Splits the data again from Validate_Test into 50% Validate and 50% Test
+    Returns the split data as train, validate, and test
+    """
     #first split
-    train, validate_test = train_test_split(telco_df, #send in initial df
-                train_size = 0.60, #size of the train df, and the test size will default to 1-train_size
-                random_state = 123, #set any number here for consistency
-                stratify = telco_df[col] #we should stratify on our target variable
+    train, validate_test = train_test_split(df,
+                train_size = 0.60, 
+                random_state = 123, 
+                stratify = df[col] 
                 )
     
     #second split
-    validate, test = train_test_split(validate_test, #we are spliting the 40% df we just made
-                test_size = 0.50, #split 50/50
-                random_state = 123, #gotta send in a random seed
-                stratify = validate_test[col] #still got to stratify
+    validate, test = train_test_split(validate_test,
+                test_size = 0.50,
+                random_state = 123,
+                stratify = validate_test[col]
                 )
-    
     return train, validate, test
+
+def telco_encoded(train, validate, test):
+    """
+    Starts an empty list as 'encoded_dfs'
+    Takes train, validate, and test into the for loop
+    Creates a copy of the df under 'df_encoded'
+    Runs another for loop in the original pulling every column from each of the dataframes
+    If column is 'customer_id' it skips past
+    If column is 'total_charges' it skips past
+    For the rest of the columns if it's an object it goes through the pd.get_dummies code if not it skips through
+    df_dummies takes the column, gives it the same name, drops the first column, and sets it as an int
+    df_encoded joins with df_dummies and drops the original column it took the data from'
+    df_encoded gets appended into encoded_dfs and gets returned
+    """
+    encoded_dfs = []
+    for df in [train, validate, test]:
+        df_encoded = df.copy()
+        for col in df.columns:
+            if col == 'customer_id':
+                continue
+            if col == 'total_charges':
+                continue
+            elif df[col].dtype == 'O':  
+                df_dummies = pd.get_dummies(df[col], prefix=col, drop_first=True).astype(int)
+                df_encoded = df_encoded.join(df_dummies).drop(columns=[col])
+        encoded_dfs.append(df_encoded)
+    return encoded_dfs
