e8ad77bd0fbcd2ddd8d10aafcc38a7877df81f36
Jacob-Ball22 <jeball22@gmail.com>
2023-12-05 16:21:16 -0600

    Updated explore functions

--- explore.py
+++ explore.py
@@ -40,14 +40,17 @@ def get_charge_tenure_chart(train_encoded):
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(11, 5))
     sns.lineplot(data=train_encoded,x='tenure', y='monthly_charges', hue='churn_Yes', ax=axes[0])
     sns.kdeplot(data=train_encoded,x='tenure', y='monthly_charges',hue='churn_Yes', ax=axes[1], fill=True, alpha=0.65)
+    fig.suptitle("Churn On Monthly Charges Over Tenure", fontsize=15)
+    fig.subplots_adjust(top=0.85)
     return(plt.show())
 
 def manwhit_charge_tenure_chart(train_encoded):
+    alpha = 0.05
     group1 = train_encoded[train_encoded['churn_Yes'] == 0]['monthly_charges']
     group2 = train_encoded[train_encoded['churn_Yes'] == 1]['monthly_charges']
     u_stat, p_value = mannwhitneyu(group1, group2)
     if p_value < alpha:
-        return print('Reject the null hypothesis'\
+        return print('Reject the null hypothesis\n'\
                      f'u_stat = {u_stat:.4f}\n'\
                      f'p= {p_value:.4f}')
     else:
@@ -55,33 +58,37 @@ def manwhit_charge_tenure_chart(train_encoded):
                      f'u_stat = {u_stat:.4f}\n'\
                      f'p= {p_value:.4f}')
 
-def senior_citizen_charts(train_encoded):
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(11, 5))
-    sns.boxplot(data=train_encoded,x='senior_citizen', y='monthly_charges', hue='churn_Yes', ax=axes[0])
-    sns.boxplot(data=train_encoded,x='senior_citizen', y='tenure', hue='churn_Yes', ax=axes[1])
-    axes[0].legend(title='Churn_Yes', loc='upper right')
-    axes[1].legend(title='Churn_Yes', loc='upper right')
+def senior_citizen_churn_chart(train_encoded):
+    sns.countplot(train_encoded, x='senior_citizen', hue = 'churn_Yes')
+    plt.title("Churn Compared To If Senior Citizen", fontsize=15)
     return(plt.show())
 
-def manwhit_senior_citizen_charges(train_encoded):
-    group1 = train_encoded[train_encoded['senior_citizen'] == 0]['monthly_charges']
-    group2 = train_encoded[train_encoded['senior_citizen'] == 1]['monthly_charges']
-    u_stat, p_value = mannwhitneyu(group1, group2)
-    if p_value < alpha:
-        return print('Reject the null hypothesis'\
-                     f'u_stat = {u_stat:.4f}\n'\
-                     f'p= {p_value:.4f}')
+def tenure_churn_chart(train_encoded):
+    train_encoded['tenure_binned'] = (train_encoded['tenure'] // 5) 
+    sns.histplot(train_encoded, x="tenure_binned",hue='churn_Yes', kde=True)
+    plt.title("Churn Compared To Tenure", fontsize=15)
+    return(plt.show())
+
+def chi2_senior_citizen(train_encoded):
+    alpha = 0.05
+    observed = pd.crosstab(train_encoded.senior_citizen,train_encoded.churn_Yes)
+    chi2, p, dof, expected = stats.chi2_contingency(observed)
+    if p < alpha:
+        print('Reject the null hypothesis\n'\
+                    f'chi^2 = {chi2:.4f}\n'\
+                    f'p= {p:.4f}')
     else:
-        return print('Fail to reject the null hypothesis\n'\
-                     f'u_stat = {u_stat:.4f}\n'\
-                     f'p= {p_value:.4f}')
+        print('Fail to reject the null hypothesis\n'\
+                    f'chi^2 = {chi2:.4f}\n'\
+                    f'p= {p:.4f}')
     
-def ttest_senior_citizen_tenure(train_encoded):
-    group1 = train_encoded[train_encoded['senior_citizen'] == 0]['tenure']
-    group2 = train_encoded[train_encoded['senior_citizen'] == 1]['tenure']
+def ttest_tenure(train_encoded):
+    alpha = 0.05
+    group1 = train_encoded[train_encoded['churn_Yes'] == 0]['tenure']
+    group2 = train_encoded[train_encoded['churn_Yes'] == 1]['tenure']
     t_stat, p_value = ttest_ind(group1, group2)
     if p_value < alpha:
-        return print('Reject the null hypothesis'\
+        return print('Reject the null hypothesis\n'\
                      f't_stat = {t_stat:.4f}\n'\
                      f'p= {p_value:.4f}')
     else:
@@ -94,7 +101,7 @@ def chi2_senior_citizen_churn(train_encoded):
     observed = pd.crosstab(train_encoded.senior_citizen,train_encoded.churn_Yes)
     chi2, p, dof, expected = stats.chi2_contingency(observed)
     if p < alpha:
-        return print('Reject the null hypothesis'\
+        return print('Reject the null hypothesis\n'\
                      f'chi^2 = {chi2:.4f}\n'\
                      f'p= {p:.4f}')
     else:
